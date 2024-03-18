95ea9262c1bb45ba16457879d811a74c2767a6b0
Nathanabrahao <nathanntc6@gmail.com>
2024-03-18 12:01:38 -0300

    commit: Alterações e atualizações de arquivos e conclusão do curso

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,26 +1,18 @@
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false
 
-    constructor(seletor: string, escapar: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
-        if(elemento){
+        if (elemento) {
             this.elemento = elemento as HTMLElement;
-        }else{
-            throw Error(`Seletor ${seletor} não existe no DOM`)
-        }
-
-        if(escapar){
-            this.escapar = escapar;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
     }
 
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-           template = template.replace(/<script>[\s\S]*?<script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 
